DeuxHuitHuit/cache_management

commit bb2fb7218cf1d604041013eab72f4fef90c8dd75

added NavigationPreRender handler. Fixes #6

extension.driver.php
@@ -21,6 +21,17 @@
 		const EXT_NAME = 'Cache Management';
 
 		/*********** DELEGATES ***********************/
+		
+		public function getSubscribedDelegates(){
+			return array(
+				array(
+					'page' => '/backend/',
+					'delegate' => 'NavigationPreRender',
+					'callback' => 'navigationPreRender'
+				)
+			);
+		}
+		
 		/**
 		 * Delegate fired to add a link to Cache Management
 		 */
@@ -42,6 +53,14 @@
 						'limit' => $group,
 					) // nav group
 			); // nav
+		}
+		
+		public function navigationPreRender($context) {
+			foreach ($context['navigation'] as $key => $section) {
+				if ($section['name'] == 'System') {
+					$context['navigation'][$key]['class'] = 'active';
+				}
+			}
 		}
 
 

commit 279b21a7a2eb16c7b6055446dd2a6e1ffc5731c8

Only highlight the System nav when in our page
This fixes a bug introduced in bb2fb72 where the System nav group would ALWYAS be active.

Now we check if the current page driver is our content page before selecting anything.

extension.driver.php
@@ -56,9 +56,12 @@
 		}
 		
 		public function navigationPreRender($context) {
-			foreach ($context['navigation'] as $key => $section) {
-				if ($section['name'] == 'System') {
-					$context['navigation'][$key]['class'] = 'active';
+			$c = Administration::instance()->getPageCallback();
+			if ($c['driver'] == 'cache_management') {
+				foreach ($context['navigation'] as $key => $section) {
+					if ($section['name'] == 'System') {
+						$context['navigation'][$key]['class'] = 'active';
+					}
 				}
 			}
 		}