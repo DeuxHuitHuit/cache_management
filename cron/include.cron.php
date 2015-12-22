70a94b45efe2e7a22ee8fd585a14ac538a9fdb8c
Nicolas Brassard <nico@nitriques.com>
2015-12-22 12:46:14 -0500

    fix cron includes for newer Symphonies

--- cron/include.cron.php
+++ cron/include.cron.php
@@ -2,11 +2,17 @@
 
 define('DOCROOT', str_replace('/extensions/cache_management/cron', '', rtrim(dirname(__FILE__), '\\/') ));
 
-require_once(DOCROOT . '/symphony/lib/boot/bundle.php');
-require_once(DOCROOT . '/symphony/lib/core/class.cacheable.php');
-require_once(DOCROOT . '/symphony/lib/core/class.symphony.php');
-require_once(DOCROOT . '/symphony/lib/core/class.administration.php');
-require_once(DOCROOT . '/symphony/lib/toolkit/class.general.php');
+if (file_exists(DOCROOT . '/vendor/autoload.php')) {
+	require_once(DOCROOT . '/vendor/autoload.php');
+	require_once(DOCROOT . '/symphony/lib/boot/bundle.php');
+}
+else {
+	require_once(DOCROOT . '/symphony/lib/boot/bundle.php');
+	require_once(DOCROOT . '/symphony/lib/core/class.cacheable.php');
+	require_once(DOCROOT . '/symphony/lib/core/class.symphony.php');
+	require_once(DOCROOT . '/symphony/lib/core/class.administration.php');
+	require_once(DOCROOT . '/symphony/lib/toolkit/class.general.php');
+}
 
 // creates the DB
 Administration::instance();
