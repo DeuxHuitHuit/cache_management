0c163027a2079387187ea3e3f83562174c935506
Alexandra Nantel <alexandra@deuxhuithuit.com>
2018-06-15 14:05:16 -0400

    Code refactoring for Database

--- lib/class.cachemanagement.php
+++ lib/class.cachemanagement.php
@@ -1,18 +1,18 @@
-<?php 
+<?php
 	/*
 	Copyight: Deux Huit Huit 2013
 	License: MIT, http://deuxhuithuit.mit-license.org
 	*/
 
 	class CacheManagement {
-		
+
 		public static function deleteFileCache() {
 			return self::purgeFileCache(false);
 		}
 		public static function purgeFileCache($expiredOnly = true, $filter = null, $subdirectory = '') {
 			$count = 0;
 			$files = General::listStructure(CACHE . $subdirectory, $filter, false);
-		
+
 			if (!empty($files)) {
 				foreach ($files['filelist'] as $file) {
 					if (!$expiredOnly || filemtime($file) < time() - (60 * 60)) {
@@ -24,22 +24,32 @@
 			}
 			return $count;
 		}
-		
+
 		/** Database */
 		public static function deleteDBCache() {
 			$count = self::getCacheCount();
-			Symphony::Database()->delete('tbl_cache', '1=1');
+
+			Symphony::Database()
+				->delete('tbl_cache')
+				->all()
+				->finalize()
+				->execute()
+				->success();
 			return $count;
 		}
-		
+
 		public static function purgeDBCache() {
 			$count = self::getCacheCount();
 			$cache = new Cacheable(Symphony::Database());
 			$cache->clean();
 			return $count - self::getCacheCount();
 		}
-		
+
 		private static function getCacheCount() {
-			return Symphony::Database()->fetchVar('c', 0, 'SELECT count(*) as `c` FROM tbl_cache');
+			return Symphony::Database()
+				->select(['count(*)' => 'c'])
+				->from('tbl_cache')
+				->execute()
+				->variable('c');
 		}
 	}
