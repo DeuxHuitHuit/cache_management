f696095dd37a85c1318c8457a74cbabc08ceea93
Alexandra Nantel <alexandra@deuxhuithuit.com>
2018-07-12 16:11:04 -0400

    Add French translation.

---
+++ lang/lang.fr.php
@@ -0,0 +1,65 @@
+<?php
+
+	$about = array(
+		'name' => 'Français',
+		'author' => array(
+			'name' => 'Alexandra Nantel',
+			'email' => 'alexandra@deuxhuithuit.co',
+			'website' => ''
+		),
+		'release-date' => '2018-07-12'
+	);
+
+	/**
+	 * Cache Management
+	 */
+	$dictionary = array(
+
+		// Missing
+
+		'Cache Management' =>
+		false,
+
+		'Please choose what to delete' =>
+		'Veuillez sélectionner quoi supprimer',
+
+		'Removing will delete expired entries. Clearing will delete everything.' =>
+		'Supprimer va effacer les entrées expirées. Vider va tout effacer.',
+
+		'Remove expired file cache' =>
+		'Supprimer la cache fichier expirée',
+
+		'Remove expired DB cache' =>
+		'Supprimer la cache BD expirée',
+
+		'Clear file cache' =>
+		'Vider la cache fichier',
+
+		'Clear DB cache' =>
+		'Vider la cache BD',
+
+		'Clear cachelite files' =>
+		'Vider les fichiers cachelite',
+
+		'Clear Cacheable Datasource files' =>
+		'Vider les fichiers Cacheable Datasource',
+
+		'All %d cachelite files deleted.' =>
+		'Tous les %d fichiers cachelite supprimés.',
+
+		'All %d files in cache deleted.' =>
+		'Tous les %d fichiers en cache supprimés.',
+
+		'Deleted %d expired files.' =>
+		'%d fichiers expirés supprimés.',
+
+		'All %d entries in cache deleted.' =>
+		'Toutes les %d entrées en cache supprimées.',
+
+		'Deleted %d expired cache entries.' =>
+		'%d entrées en cache expirées supprimées.',
+
+		'All %d Cacheable Datasource files deleted.' =>
+		'Tous les %d fichiers Cacheable Datasource supprimés.',
+
+	);
